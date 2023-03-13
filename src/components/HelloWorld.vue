96c4f601d77dfdba522e4a2bd2253526c87013ba
lshbosheth <lizezhi@jk366.net>
2023-03-13 19:21:48 +0800

    fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
 defineProps<{ msg: string }>()
-
-const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="title">{{msg}}</div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.title {
+  font-size: 30px;
 }
 </style>
